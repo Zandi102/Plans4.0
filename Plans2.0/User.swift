07b1572c7bc4cc730cbe11391c3896aa35292edd
Alexander Pallozzi <alexanderpallozzi@gmail.com>
2022-05-20 16:28:12 -0400

    Added Constraints to map and fixed bug with automatic deletion

--- Plans2.0/User.swift
+++ Plans2.0/User.swift
@@ -278,15 +278,16 @@ class User : Identifiable {
                 //print(jsonData3)
                 let resp: PlanStruct = try! JSONDecoder().decode(PlanStruct.self, from: jsonData)
                 print(resp.plan_name)
-                if(Plan.textToDate(resp.date).compare(Date().addingTimeInterval(-86400)).rawValue < 0) {
+                //Date().
+                if(Plan.textToDate(resp.date).compare(Date().addingTimeInterval(-100000)).rawValue < 0 && Plan.dayText(Plan.textToDate(resp.date)) != Plan.dayText(Date())) {
                     //print(resp.plan_name + " is" + " completed");
                     let db = DBManager();
                     let url = URL(string: "http://abdasalaam.com/Functions/deletePlan.php")!
                     let parameters: [String: Any] = [
                         "plan_id": resp.plan_id,
                     ]
-                    let message = db.postRequest(url, parameters)
-                    print(message);
+                    let message3 = db.postRequest(url, parameters)
+                    print(message3);
                 }
                 else {
                     plans.append(Plan(title: resp.plan_name, day:Plan.textToDate(resp.date), startTime: Plan.textToTime(resp.startTime), endTime:Plan.textToTime(resp.endTime), address: resp.address, notes: resp.description, ownerUsername: resp.username, plan_id: resp.plan_id))
@@ -301,18 +302,15 @@ class User : Identifiable {
                 //print(jsonData3)
                 let resp: PlanStruct = try! JSONDecoder().decode(PlanStruct.self, from: jsonData2)
                 print(resp.plan_name)
-                if(Plan.textToDate(resp.date).compare(Date()).rawValue < 0 && Plan.dayText(Plan.textToDate(resp.date)) != Plan.dayText(Date())){
-                    //print(Plan.textToDate(resp.date))
-                    //&& Plan.dayText(Plan.textToDate(resp.date)) != Plan.dayText(Date())
+                if(Plan.textToDate(resp.date).compare(Date().addingTimeInterval(-100000)).rawValue < 0 && Plan.dayText(Plan.textToDate(resp.date)) != Plan.dayText(Date())){
                     //print(resp.plan_name + " is" + " completed");
-//                    //delete plan from database
-//                    let db = DBManager();
-//                    let url = URL(string: "http://abdasalaam.com/Functions/deletePlan.php")!
-//                    let parameters: [String: Any] = [
-//                        "plan_id": resp.plan_id,
-//                    ]
-//                    let message = db.postRequest(url, parameters)
-//                    print(message);
+                    let db = DBManager();
+                    let url = URL(string: "http://abdasalaam.com/Functions/deletePlan.php")!
+                    let parameters: [String: Any] = [
+                        "plan_id": resp.plan_id,
+                    ]
+                    let message4 = db.postRequest(url, parameters)
+                    print(message4);
                 }
                 else {
                     plans.append(Plan(title: resp.plan_name, day:Plan.textToDate(resp.date), startTime: Plan.textToTime(resp.startTime), endTime:Plan.textToTime(resp.endTime), address: resp.address, notes: resp.description, ownerUsername: resp.username, plan_id: resp.plan_id))
