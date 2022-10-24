f9c810ae06567287800fcd55d5f989503ba9c720
Alexander Pallozzi <alexanderpallozzi@gmail.com>
2022-10-24 00:54:26 -0400

    Commit for fixing time issue on create plan

--- Plans2.0/CreatePlanViewController.swift
+++ Plans2.0/CreatePlanViewController.swift
@@ -124,9 +124,10 @@ class CreatePlanViewController: UIViewController, UITextFieldDelegate, UITextVie
     private func validate(planToValidate : Plan) {
         // the return value could either be a plan or nil value
         // validate the start time and end time of the plan, to make sure that start time is > current date, and end time is > starttime
-        if (planToValidate.day.compare(Date.now).rawValue > 0 && planToValidate.startTime.compare(Date.now).rawValue > 0) || (Plan.dayText(planToValidate.day) == Plan.dayText(Date.now) && planToValidate.startTime.compare(Date.now).rawValue > 0) {
+        if planToValidate.day.compare(Date.now).rawValue > 0 || (Plan.dayText(planToValidate.day) == Plan.dayText(Date.now) && planToValidate.startTime.compare(Date.now).rawValue > 0) {
             // validate the address string input of the plan
             // sample address: 11317 Bellflower Road, Cleveland, OH 44106
+            print(planToValidate.startTime);
             valid_coord(plan: planToValidate) { (complete, error) in
                 if error == nil {
                     if(self.isTimeInvalid == true) {
@@ -137,23 +138,18 @@ class CreatePlanViewController: UIViewController, UITextFieldDelegate, UITextVie
                         self.checkAddressInput.removeFromSuperview()
                         self.failPlan.removeFromSuperview()
                     }
-                    let planName1 = planToValidate.title
-                    let datePicker1 = planToValidate.day
-                    let startPicker1 = planToValidate.startTime.addingTimeInterval(-3600 * 4 + 60)
-                    let endPicker1 = planToValidate.endTime.addingTimeInterval(-3600 * 4 + 60)
-                    let addressName = planToValidate.address!
-                    let planNotes1 = planToValidate.notes
+                    print(planToValidate.startTime)
+                    
                     let db = DBManager();
                     let url : URL = URL(string: "http://abdasalaam.com/Functions/createPlan.php")!
-                    let username = User.currentUser.userName
-                        let parameters: [String: Any] = [
-                            "plan_name":planName1,
-                            "startTime":startPicker1,
-                            "endTime":endPicker1,
-                            "date":datePicker1,
-                            "address":addressName,
-                            "description":planNotes1,
-                            "username": username
+                    let parameters: [String: Any] = [
+                            "plan_name":planToValidate.title,
+                            "startTime":Plan.timeText(planToValidate.startTime),
+                            "endTime":Plan.timeText(planToValidate.endTime),
+                            "date":planToValidate.day.description,
+                            "address":planToValidate.address!,
+                            "description":planToValidate.notes,
+                            "username": User.currentUser.userName
                         ]
                     _ = db.postRequest(url, parameters)
                     planToValidate._coord = CLLocationCoordinate2D(latitude: complete.latitude, longitude: complete.longitude)
@@ -238,9 +234,9 @@ class CreatePlanViewController: UIViewController, UITextFieldDelegate, UITextVie
     @objc func createPlan() {
         if(add_success == false) {
             let day : Date = self.datePicker.date
-            let dayDifference : TimeInterval = day.timeIntervalSince(Date())
-            let startTime : Date = self.startTimePicker.date.addingTimeInterval(dayDifference)
-            let endTime   : Date = self.endTimePicker.date.addingTimeInterval(dayDifference)
+            //let dayDifference : TimeInterval = day.timeIntervalSince(Date())
+            let startTime : Date = self.startTimePicker.date
+            let endTime   : Date = self.endTimePicker.date
             let planToAdd = Plan(title: planName.text!, day: day, startTime: startTime, endTime: endTime, address: planAddress.text!, notes: planNotes.text!, ownerUsername: User.currentUser.userName)
             
             validate(planToValidate: planToAdd)
